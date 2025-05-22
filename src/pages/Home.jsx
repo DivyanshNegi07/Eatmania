f7c9c846431547d0aafb8696c4ae3053b6c4ef2f
DivyanshNegi07 <divyanshnegi884@gmail.com>
2025-05-22 12:38:33 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/header"
+import Header from "../components/Header"
 import Categories from "../components/Categories"
 import "../styles/Home.css"
 import React,{useRef} from 'react'
